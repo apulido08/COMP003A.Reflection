64f31034e14553051d340f775356849b9ca4cadd
adodoCoder <alavropulido@my.whccd.edu>
2025-02-24 00:47:08 -0800

    Add project files.

---
+++ COMP003A.Reflection/Program.cs
@@ -0,0 +1,34 @@
+﻿// Author: [Alvaro Pulido]
+// Course: COMP003A
+// Faculty: Jonathan Cruz
+// Purpose: Reflection
+
+namespace COMP003A.Reflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What worked well for you over the last 8 weeks?");
+            Console.WriteLine();
+
+            Console.WriteLine("What external resources did you use to aid your learning?");
+            Console.WriteLine();
+
+            Console.WriteLine("What would you change about the course?");
+            Console.WriteLine();
+
+            Console.WriteLine("Did you learn what you expected to learn in this course?");
+            Console.WriteLine();
+
+            Console.WriteLine("What concepts were new to you?");
+            Console.WriteLine();
+
+            Console.WriteLine("What concepts have you encountered already in the past?");
+            Console.WriteLine();
+
+            Console.WriteLine("Do you have any tips/comments for the instructor?");
+            Console.WriteLine();
+        }
+    }
+}
