b740b85a264f169481d1d1dc7f089ae4cf2ddde7
adodoCoder <alavropulido@my.whccd.edu>
2025-02-26 19:58:52 -0800

    Added answers to the questions

--- COMP003A.Reflection/Program.cs
+++ COMP003A.Reflection/Program.cs
@@ -10,25 +10,25 @@ namespace COMP003A.Reflection
         static void Main(string[] args)
         {
             Console.WriteLine("What worked well for you over the last 8 weeks?");
-            Console.WriteLine();
+            Console.WriteLine("What helped me the most was that the lectures and assignments had hints and walkthroughs.");
 
             Console.WriteLine("What external resources did you use to aid your learning?");
-            Console.WriteLine();
+            Console.WriteLine("The esternal resource that helped the most was W3 schools as well as some of the links in the lectures.");
 
             Console.WriteLine("What would you change about the course?");
-            Console.WriteLine();
+            Console.WriteLine("I would change the lenght of the course. Push it out to maybe 3-4 months.");
 
             Console.WriteLine("Did you learn what you expected to learn in this course?");
-            Console.WriteLine();
+            Console.WriteLine("I learned alot more than what I expected to leran in this class.");
 
             Console.WriteLine("What concepts were new to you?");
-            Console.WriteLine();
+            Console.WriteLine("The entire class felt new to me since I had no idea what I was getting into. Alot of ther material was fun to learn.");
 
             Console.WriteLine("What concepts have you encountered already in the past?");
-            Console.WriteLine();
+            Console.WriteLine("Some of the concepts that ive emncountered already were the variables, types, and oeprators.");
 
             Console.WriteLine("Do you have any tips/comments for the instructor?");
-            Console.WriteLine();
+            Console.WriteLine("Some tips would be to keep adjusting the lectures to how trhis class was. It was alot of help when doing the coding assignments. ");
         }
     }
 }
